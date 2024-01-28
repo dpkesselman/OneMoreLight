a613f9e574ac813973a31c4b3c768e1e01f0ae14
Daniela Kesselman <d.p.kesselman@gmail.com>
2024-01-28 01:43:16 -0300

    NPC collisions with problems

--- Assets/Scripts/CollisionController.cs
+++ Assets/Scripts/CollisionController.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class CollisionController : MonoBehaviour
 {
     public AudioSource tickles;
-
+    public SpriteRenderer mySpriteRenderer;
+    public Sprite temp;
+    
     void Start()
     {
         tickles = GetComponent<AudioSource>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -32,6 +35,9 @@ public class CollisionController : MonoBehaviour
     {
         if (collision.gameObject.tag == "NPC")
         {
+            temp = collision.gameObject.GetComponent<SpriteRenderer>().sprite;
+            collision.gameObject.GetComponent<SpriteRenderer>().sprite = mySpriteRenderer.sprite;
+            mySpriteRenderer.sprite = temp;
             Debug.Log("Exit");
         }   
     }
