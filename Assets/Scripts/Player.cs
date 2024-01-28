094e8cfa91f720de18dbb9f8af17eb051609e992
Ivan Pagano <ivanpgn87@gmail.com>
2024-01-28 13:51:32 -0300

    - Agregadas animaciones de movimiento - Arreglado Exit time del animator - Agregada plataforma de ejemplo

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,8 @@ public class Player : MonoBehaviour
     public LayerMask groundLayer;
     // para ver que grande es nuestro grounChek
     public float groundCheckRadius;
+    
+    [SerializeField] private Animator myAnimationController;
 
     public float speed = 2.0f;
     public float movX;
@@ -29,8 +31,31 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movX = transform.position.x + Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        // Calculamos el movimiento en base al input. Esto es, cuanta distancia debe moverse el gatito en este frame.
+        float currentMovement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        
+        movX = transform.position.x + currentMovement;
         movY = transform.position.y;
+        
+        // Rotación del gatito
+        if (currentMovement > 0) // Camina hacia la derecha
+        {
+            transform.localScale = new Vector3(0.2f, 0.2f, 1);
+        }
+        else if (currentMovement < 0) // Camina hacia la izquierda
+        {
+            transform.localScale = new Vector3(-0.2f, 0.2f, 1);
+        }
+        
+        // Animación movimiento
+        if (currentMovement != 0)
+        {
+            myAnimationController.SetBool("playRun", true);
+        }
+        else
+        {
+            myAnimationController.SetBool("playRun", false);
+        }
 
         // Est� en el piso?
         // suelta peque�as bolas en el pto y radio que le digamos y check contra una layer. Si encuentra + de una layer devuelve un true
